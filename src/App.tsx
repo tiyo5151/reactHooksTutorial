a51e86759417530d46928b8b42852f2063f093d2
tiyo5151 <s1F102400224@iniad.org>
2024-09-06 22:29:16 +0900

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState<number>(0);
+
+  const handleClick = () => {
+    setCount(count + 1);
+    console.log(count);
+    return count;
+  };
+  return (
+    <div className="App">
+      <h1>UseState</h1>
+      <button onClick={handleClick}>+</button>
+      <p>{count}</p>
+    </div>
+  );
+}
+
+export default App;
