6c3710da22d157e1c21b391e00dd090af8b5f017
tiyo5151 <s1F102400224@iniad.org>
2024-09-07 23:59:44 +0900

    useeffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
@@ -9,6 +9,11 @@ function App() {
     console.log(count);
     return count;
   };
+
+  useEffect(() => {
+    console.log('Hello Hooks');
+  }, [count]);
+
   return (
     <div className="App">
       <h1>UseState</h1>
